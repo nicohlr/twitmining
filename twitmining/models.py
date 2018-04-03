26208c0512c1c0de7a66eb147688665f6248a27a
Nicolas <nicolas.houlier@gmail.com>
2018-04-03 22:02:36 +0200

    Adding a model and a view for Tweet

--- twitmining/models.py
+++ twitmining/models.py
@@ -1,27 +1,15 @@
 from django.db import models
-import twitmining.config as config
-import requests
-
-
 # Create your models here.
-class Tweets(models.Model):
+
+
+class Tweet(models.Model):
     user = models.CharField(max_length=30)
-    date = models.DateTimeField()
+    date = models.CharField(max_length=30)
     content = models.CharField(max_length=140)
     hashtags = models.CharField(max_length=140)
+    number = models.IntegerField()
 
     def __str__(self):
-        return self.date
+        return 'Tweet number {0}, written by {1} on {2} : {3} {4}'.format(self.number, self.user, self.date,
+                                                                          self.content, self.hashtags)
 
-    @staticmethod
-    def get_tweet(keyword):
-        tweets = []
-        headers = config.search_headers
-        search_params = {'q': keyword, 'result_type': 'recent', 'count': 1}
-        rsp = requests.get(config.search_url, headers=headers, params=search_params).json()
-        for x in rsp['statuses']:
-            tweets.append({'user': x['entities']['user_mentions'][0]['screen_name'],
-                           'date': x['created_at'],
-                           'content': x['text'],
-                           'hashtags': x['entities']['hashtags']})
-        return tweets
--- twitmining/views.py
+++ twitmining/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from twitmining.models import Tweets
-
-
+from twitmining.models import Tweet
+import twitmining.config as config
+import requests
 # Create your views here.
 
 
@@ -15,5 +15,10 @@ def home(request):
 
 
 def tweets(request):
-    list_tweets = Tweets.get_tweet('PSG')
-    return HttpResponse(list_tweets)
+    headers = config.search_headers
+    search_params = {'q': 'PSG', 'result_type': 'recent', 'count': 1}
+    rsp = requests.get(config.search_url, headers=headers, params=search_params).json()
+    for x in rsp['statuses']:
+        Tweet.objects.create(user=x['user']['name'], date=x['created_at'],
+                             content=x['text'], number=0).save()
+    return HttpResponse(Tweet.objects.all())

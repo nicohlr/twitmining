c36d20ad2fe31d211c4e0550c56ff9dd3c71cf99
Nicolas <nicolas.houlier@gmail.com>
2018-04-08 12:33:24 +0200

    empty DB before each query

--- twitmining/urls.py
+++ twitmining/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('query/', views.query, name='query'),
-    path('empty/', views.empty_database, name='empty')
 ]
--- twitmining/views.py
+++ twitmining/views.py
@@ -10,16 +10,14 @@ def home(request):
 
 
 def query(request):
-    tweets = t.search.tweets(q="vache", count=10)
+    Tweet.objects.all().delete()
+    tweets = t.search.tweets(q="django", count=10)
     links = []
     count = 0
     for tweet in tweets['statuses']:
         Tweet.objects.create(id_number=tweet['id_str']).save()
-        links += ['https://twitter.com/TheTwitmining/status/' + str(Tweet.objects.all()[count])]
+        links += ['https://twitter.com/TheTwitmining/status/' +
+                  str(Tweet.objects.all()[count])]
         count += 1
     return render(request, './twitmining/query.html', {'links': links})
 
-
-def empty_database(request):
-    Tweet.objects.all().delete()
-    return render(request, './twitmining/empty.html')

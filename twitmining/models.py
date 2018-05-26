7a65d33105c7fa4cfe4830e4b5c0e2d39f025ea7
Nicolas <nicolas.houlier@gmail.com>
2018-05-26 20:32:12 +0200

    Add a scoring method to class SearchEngine

--- twitmining/models.py
+++ twitmining/models.py
@@ -4,9 +4,20 @@ from django.db import models
 
 class Tweet(models.Model):
     id_number = models.CharField(max_length=30)
+    text = models.CharField(max_length=140)
+    hashtags = models.CharField(max_length=140).choices
+    user_mentions = models.CharField(max_length=140).choices
+    verified = models.BooleanField()
+    location = models.CharField(max_length=30)
+    link = models.CharField(max_length=60)
+    score = models.PositiveSmallIntegerField()
 
     def __str__(self):
         return self.id_number
+
+
+class RelevantTweet(models.Model):
+    link = models.CharField(max_length=60)
 
 
 class Keyword(models.Model):
--- twitmining/util/search.py
+++ twitmining/util/search.py
@@ -1,6 +1,36 @@
+from twitmining.models import Tweet, Keyword, RelevantTweet
+
+
 class SearchEngine:
 
     def __init__(self):
-        self.relevants = list()
+        self.tweets = Tweet.objects.all()
+        self.keyword = str(Keyword.objects.all()[0])
 
-    def
+    def score_tweets(self):
+
+        for tweet in Tweet.objects.all():
+
+            tweet.score += tweet.count(self.keyword)
+
+            for hashtag in tweet.hashtag:
+
+                if hashtag.count(self.keyword) != 0:
+
+                    tweet.score += 2
+
+            if tweet.user_mentions is not None:
+
+                tweet.score += 2
+
+            if tweet.verified:
+
+                tweet.score += 5
+
+            if tweet.location != "":
+
+                tweet.score += 5
+
+            if tweet.score > 10:
+
+                RelevantTweet.objects.create(link=tweet.link)

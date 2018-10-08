a3a86d6ffe4097f30c5652a96dab688bfeb84c29
Nicolas <nicolas.houlier@gmail.com>
2018-10-08 12:05:03 +0200

    update imports after modification of database

--- twitmining/admin.py
+++ twitmining/admin.py
@@ -1,8 +1,4 @@
 from django.contrib import admin
-from .models import Tweet, Keyword
-# Register your models here.
+from .models import Keyword
 
-admin.site.register(Tweet)
 admin.site.register(Keyword)
-
-
--- twitmining/util/search.py
+++ twitmining/util/search.py
@@ -1,6 +1,4 @@
 import pandas as pd
-from twitmining.models import Tweet, RelevantTweet
-
 
 class SearchEngine:
     """
